635fde61dc62eb6cf745c50f1e7705d8bdee71c6
gijslebesque <gijslebesque@gmail.com>
2019-09-12 21:34:32 +0200

    fixed minior bug

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
-app.use("/profile", authenticateRouter, profileRouter);
+
+//before we reach profileRouter function, we use our custom middleware
+app.use("/profile", authorisationRouter, profileRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/authorisation.js
+++ routes/authorisation.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+//Our custom middleware function.
 router.use("/", (req, res, next) => {
   if (req.session.currentUser) {
     next();
