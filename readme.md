c61ceb7536b030d53e6f96ae8cc47b316f3055d6
gijslebesque <gijslebesque@gmail.com>
2019-09-25 17:24:20 +0200

    work in progress

---
+++ readme.md
@@ -0,0 +1,4 @@
+#Steps for installing passport local
+
+1. npm install passport passport-local
+2. create a config directory and inside there a passport.js where we'll declare how our local strategy will handle authorisation
--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
+const User = require('../models/User');
 
-router.get("/", (req, res) => {
-  User.findById(req.session.user._id)
-    .populate("posts") // here mongoose also queries the posts collection
+router.get('/', (req, res) => {
+  debugger;
+  User.findById(req.user._id)
+    .populate('posts') // here mongoose also queries the posts collection
     .then(user => {
       debugger;
-      res.render("profile", { user: user });
+      res.render('profile', { user: user });
     });
   //mongoose looks for user with id of session.user._id
   //populate is gonna look for all the posts that match id with whatever is in posts array
